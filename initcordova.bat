dff0b4cb60da117e7647cdf7de100619dd517b77
mulargui <mauricio@ulargui.com>
2015-07-28 10:47:33 -0700

    adding device data

--- initcordova.bat
+++ initcordova.bat
@@ -2,5 +2,7 @@ cd C:\mauricio\Dropbox\vagrant\apps
 cordova create healthylinkxcordova com.ulargui.healthylinkxcordova Healthylinkx
 cd healthylinkxcordova
 cordova platform add android
+cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-geolocation.git
+cordova plugin add https://git-wip-us.apache.org/repos/asf/cordova-plugin-device.git
 rem cordova build
 rem cordova emulate android
